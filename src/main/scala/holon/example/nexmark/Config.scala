63aff0ad3f46447232fa283f6e58d1b51ca3c73f
Kolya Krafeld <k.krafeld@gmail.com>
2025-03-24 14:19:52 +0100

    Refactor filepath to GC auth file

--- src/main/scala/holon/example/nexmark/Config.scala
+++ src/main/scala/holon/example/nexmark/Config.scala
@@ -21,8 +21,11 @@ object Config {
   final val BROADCAST_PARTITION_ID = -1
   final val CONTROL_PARTITION_ID = -2
 
+  final val GC_CREDENTIALS_FILE_PATH = "/Users/kolya/kth_projects/holon-streaming/.gcp/gcs-service-account.json"
+
   final val WORK_STEALING_THRESHOLD = 10   // Determines how many (empty) polls a node should perform before it starts work stealing.
   var SLEEP_BETWEEN_POLLS = 0L
+
 
   var KAFKA_HOST = "localhost"
   var KAFKA_PORT = 9092
