684cf48e2d0096c8656b0344fd0ec64777082104
RubenvanGemeren <47777782+RubenvanGemeren@users.noreply.github.com>
2025-04-08 11:42:54 +0200

    Add GCounter serializer to proc fun factory

--- src/main/scala/holon/example/nexmark/WindowedRecordProcFunFactory.scala
+++ src/main/scala/holon/example/nexmark/WindowedRecordProcFunFactory.scala
@@ -3,18 +3,26 @@ package holon.example.nexmark
 import holon.ProcFun
 import holon.ProcFunFactory
 import holon.backend.WindowedRecordProcFun
-import holon.crdt.GCounterWrapper
-import org.apache.pekko.cluster.ddata.GCounter
+import holon.crdt.{GCounterWrapper, TestCRDTWrapper}
+import holon.example.CRDT
+import org.apache.pekko.cluster.ddata.{GCounter, SelfUniqueAddress}
 import upickle.default.*
 
 // This factory creates WindowedRecordProcFun instances using GCounter as the CRDT
 class WindowedRecordProcFunFactory extends ProcFunFactory {
   override def create(partition: Int): ProcFun = {
     implicit val wrapper: GCounterWrapper.type = GCounterWrapper
+    
     implicit val rw: ReadWriter[GCounter] = readwriter[Array[Byte]].bimap[GCounter](
-      gc => holon.example.CRDT.crdtToBinaryWithManifest("GCounter", gc),
-      bytes => holon.example.CRDT.crdtFromBinaryWithManifest(bytes)._2.asInstanceOf[GCounter]
+      gc => CRDT.crdtToBinaryWithManifest("GCounter", gc),
+      bytes => CRDT.crdtFromBinaryWithManifest(bytes)._2.asInstanceOf[GCounter]
     )
+    // This ensures that a mutable Map[String, (GCounter, Boolean)] is encoded as a dictionary.
+    implicit val windowMapRW: ReadWriter[scala.collection.mutable.Map[String, (GCounter, Boolean)]] =
+      readwriter[Map[String, (GCounter, Boolean)]].bimap(
+        (m: scala.collection.mutable.Map[String, (GCounter, Boolean)]) => m.toMap,
+        (m: Map[String, (GCounter, Boolean)]) => scala.collection.mutable.Map(m.toSeq: _*)
+      )
 
     new WindowedRecordProcFun[GCounter](partition)
   }
