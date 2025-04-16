c178c239812a32342bbd03820a17f732bc19fc5f
RubenvanGemeren <47777782+RubenvanGemeren@users.noreply.github.com>
2025-04-16 15:24:39 +0200

    Add new serialization files to contain custom serializations

---
+++ src/main/scala/holon/serialization/AuctionPopularityWindowedSerialization.scala
@@ -0,0 +1,23 @@
+package holon.serialization
+
+import holon.example.CRDT
+import org.apache.pekko.cluster.ddata.GCounter
+import upickle.legacy.{ReadWriter, readwriter}
+
+implicit val mapGcounterRW: ReadWriter[Map[String, GCounter]] =
+  readwriter[Map[String, Array[Byte]]].bimap[Map[String, GCounter]](
+    // Convert a Map[String, GCounter] into a Map[String, Array[Byte]]
+    m => m.map { case (k, gcounter) => k -> CRDT.crdtToBinaryWithManifest("GCounter", gcounter) },
+    // Convert back from Map[String, Array[Byte]] to Map[String, GCounter]
+    bytesMap => bytesMap.map { case (k, bytes) =>
+      k -> CRDT.crdtFromBinaryWithManifest(bytes)._2.asInstanceOf[GCounter]
+    }
+  )
+
+// Serialization for the window state.
+// The window state is a mutable Map keyed by Long with values of type (Map[String, GCounter], Boolean)
+implicit val auctionPopularityWindowMapRW: ReadWriter[scala.collection.mutable.Map[Long, (Map[String, GCounter], Boolean)]] =
+  readwriter[Map[Long, (Map[String, GCounter], Boolean)]].bimap(
+    (m: scala.collection.mutable.Map[Long, (Map[String, GCounter], Boolean)]) => m.toMap,
+    (m: Map[Long, (Map[String, GCounter], Boolean)]) => scala.collection.mutable.Map(m.toSeq: _*)
+  )
