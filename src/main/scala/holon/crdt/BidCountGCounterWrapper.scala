251d4b01f39f3f0ca7c05891521a3b0a479d4283
RubenvanGemeren <47777782+RubenvanGemeren@users.noreply.github.com>
2025-04-16 16:12:10 +0200

    Rename gcounter wrapper

--- src/main/scala/holon/crdt/BidCountGCounterWrapper.scala
+++ src/main/scala/holon/crdt/BidCountGCounterWrapper.scala
@@ -3,7 +3,7 @@ package holon.crdt
 import holon.example.Nexmark
 import org.apache.pekko.cluster.ddata.{GCounter, SelfUniqueAddress}
 
-object GCounterWrapper extends CRDTWrapper[GCounter] {
+object BidCountGCounterWrapper extends CRDTWrapper[GCounter] {
   override def empty: GCounter = GCounter.empty
 
   override def increment(crdt: GCounter, address: SelfUniqueAddress, delta: Nexmark.Events.Bid): GCounter =
--- src/main/scala/holon/example/nexmark/BidCountWindowedFactory.scala
+++ src/main/scala/holon/example/nexmark/BidCountWindowedFactory.scala
@@ -3,15 +3,13 @@ package holon.example.nexmark
 import holon.ProcFun
 import holon.ProcFunFactory
 import holon.backend.WindowedRecordProcFun
-import holon.crdt.GCounterWrapper
-import holon.example.CRDT
+import holon.crdt.BidCountGCounterWrapper
 import org.apache.pekko.cluster.ddata.GCounter
-import upickle.default.*
 
 // This factory creates AuctionWindowedRecordProcFun instances using AuctionGCounterWrapper
 class BidCountWindowedFactory extends ProcFunFactory {
   override def create(partition: Int): ProcFun = {
-    implicit val wrapper: GCounterWrapper.type = GCounterWrapper
+    implicit val wrapper: BidCountGCounterWrapper.type = BidCountGCounterWrapper
     
     new WindowedRecordProcFun[GCounter](partition)(
       crdt = wrapper,
