902b270cbbed812623aa2436568c55899ac9f7c4
RubenvanGemeren <47777782+RubenvanGemeren@users.noreply.github.com>
2025-05-12 06:18:24 +0200

    Added extra logging

--- src/main/scala/holon/example/nexmark/NexmarkProducer.scala
+++ src/main/scala/holon/example/nexmark/NexmarkProducer.scala
@@ -21,6 +21,8 @@ object NexmarkProducer {
         val port = kafkaBootstrapServers.split(":").last.toInt
         val PRODUCER_SLEEP_TIME_MS = sys.env.getOrElse("PRODUCER_SLEEP_TIME_MS", "100").toInt
 
+        logger.info(s"Set up the nexmark producer with these parameters: $host, $port, $PRODUCER_SLEEP_TIME_MS")
+
         while (!FirestoreClient.isStartFlagSet) {
             logger.info("Waiting for start flag to be set.")
             Thread.sleep(1_000)
@@ -37,15 +39,20 @@ object NexmarkProducer {
         // Track the amount of input events per window (even if they are not the specific CRDT class).
         val inputEventsPerWindow = mutable.Map.empty[Long, Long]
 
+        logger.debug("Starting Nexmark Producer with number of partitions: " + nrOfKafkaPartitions())
+
         while true do
             for i <- 0 until nrOfKafkaPartitions() do
+                logger.debug("creating a batch for partition: " + i)
                 val batch = (0 until PRODUCER_BATCH_SIZE)
                     .map(_ => iter.next())
                     .map(x => {
                         val window = defineWindow(x.timestamp)
                         inputEventsPerWindow(window) = inputEventsPerWindow.getOrElse(window, 0L) + 1
+                        logger.debug(s"a batch has been created for partition: $i, where binary: ${writeBinary(i).mkString("Array(", ", ", ")")}, window: $window, event: ${x.toString}")
                         (writeBinary(i), Nexmark.serialize(x))
                     })
+                logger.debug(s"sending batch for partition: $i, where batch size: ${batch.size}, batch: ${batch.mkString("Array(", ", ", ")")}")
                 producer.send(batch)
 
             producer.flush()
@@ -63,10 +70,11 @@ object NexmarkProducer {
     }
 
     def setupConfig(): Unit = {
-        val N_NODES = sys.env.getOrElse("N_NODES", "2").toInt
+        val N_NODES = sys.env.getOrElse("N_NODES", "3").toInt
         Config.N_NODES = N_NODES
         val PARTITIONS_PER_NODE = sys.env.getOrElse("PARTITIONS_PER_NODE", "2").toInt
         Config.PARTITIONS_PER_NODE = PARTITIONS_PER_NODE
+        logger.info(s"Producer set up config with N_NODES: $N_NODES and $PARTITIONS_PER_NODE partitions per node" )
     }
 
     def defineWindow(eventTime: Long): Long = {
