2586bf1363f792b160c190e263553e4fbaba9a30
RubenvanGemeren <47777782+RubenvanGemeren@users.noreply.github.com>
2025-09-01 14:43:52 +0200

    add logging to firestoreclient

--- src/main/scala/holon/backend/cloud/FirestoreClient.scala
+++ src/main/scala/holon/backend/cloud/FirestoreClient.scala
@@ -10,13 +10,15 @@ import scala.jdk.CollectionConverters.*
 
 object FirestoreClient {
 
+    val logger = Logger.apply("FirestoreClient")
+    Logger.setLevel("FirestoreClient", "INFO")
     val FLAG_COLLECTION = FIRESTORE_START_KEY
+
+    logger.info(s"Using Firestore collection: ${sys.env.getOrElse("GOOGLE_FIRESTORE_CREDENTIALS", "NOT FOUND GOOGLE FIRESTORE")}")
 
     val credentialsPath = sys.env.getOrElse("GOOGLE_FIRESTORE_CREDENTIALS", "/Users/rvang/Documents/GitHub/holon-streaming-clone/.gcp/firebase-user-account.json")
     val credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath))
     val firestore: Firestore = FirestoreOptions.newBuilder().setCredentials(credentials).build().getService
-    val logger = Logger.apply("FirestoreClient")
-    Logger.setLevel("FirestoreClient", "INFO")
 
     /**
      * Checks the `start_flag` document in the `FLAG_COLLECTION` to see if the `start` property is true or false.
